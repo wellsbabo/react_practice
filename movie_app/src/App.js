d53bf8ec657e9d8b2ad41abc1d552694912b392a
이정관 <wellsbabo@gravity.co.kr>
2023-06-26 18:59:16 +0900

    movie app part one

---
+++ movie_app/src/App.js
@@ -0,0 +1,41 @@
+import {useEffect, useState} from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+    //.then 대신에 async-await 사용
+  const getMovies = async() => {
+      const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year");
+      // 위에 내용과 같은 내용
+      // const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
+      const json = await response.json();
+      //console.log(json);
+      setMovies(json.data.movies);
+      setLoading(false);
+  };
+
+  useEffect(() => {
+        getMovies();
+  } , []);
+
+  console.log(movies);
+
+  return (
+      <div>
+          {loading ? <h1>Loading...</h1> : <div>
+              {movies.map(movie =>
+                  <div key={movie.id}>
+                      <img src={movie.medium_cover_image}/>
+                      <h2>{movie.title}</h2>
+                      <p>{movie.summary}</p>
+                      <ul>
+                          {movie.genres.map(g=> <li key={g}>{g}</li>)}
+                      </ul>
+                  </div>)}
+          </div>}
+      </div>
+  );
+}
+
+export default App;
